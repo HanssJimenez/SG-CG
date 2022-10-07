a7db8836f9d7dd0c28a9fbe21fdcf12d23b8b7c5
Hanss <hjimenezm1@miumg.edu.gt>
2022-10-07 03:28:51 -0600

    validación de errores

--- proyecto_final/principal/forms.py
+++ proyecto_final/principal/forms.py
@@ -14,8 +14,6 @@ class UserForm(forms.ModelForm):
         vp = cleaned_data['vpassword']
         if p != vp:
             raise forms.ValidationError('La constraseña no es igual')
-            
-                
 
 class UserProfileInfoForm(forms.ModelForm):
 
@@ -24,12 +22,26 @@ class UserProfileInfoForm(forms.ModelForm):
         fields = ('portfolio_site','profile_pic')
 
 class AgreProdForm(forms.ModelForm):
-   class Meta():
-     model = Inventario
-     fields = ('categoria', 'nombre_p', 'cantidad')
-     
+    class Meta():
+        model = Inventario
+        fields = ('categoria', 'nombre_p', 'cantidad')
+    def clean_categoria(self):
+        categoria = self.cleaned_data.get('categoria')
+        if not categoria:
+            raise forms.ValidationError('Completa este campo')
+        for instance in Inventario.objects.all():
+            if instance.categoria == categoria:
+                raise forms.ValidationError(str(categoria) + ' ya existe')
+        return categoria
+
+    def clean_nombre_p(self):
+        nombre_p = self.cleaned_data.get('nombre_p')
+        if not nombre_p:
+            raise forms.ValidationError('Completa este campo')
+        return nombre_p  
+
 
 class BusquedProductoForm(forms.ModelForm):
-   class Meta:
-     model = Inventario
-     fields = ['categoria', 'nombre_p']
+    class Meta:
+        model = Inventario
+        fields = ['categoria', 'nombre_p']
