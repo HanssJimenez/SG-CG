d49d697d2196b418a288b781cb6c1985ab37cea4
Hanss <hjimenezm1@miumg.edu.gt>
2022-10-25 08:56:29 -0600

    cambios

--- proyecto_final/principal/models.py
+++ proyecto_final/principal/models.py
@@ -1,6 +1,4 @@
 from email.policy import default
-from secrets import choice
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
